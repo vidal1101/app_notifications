c45ee94ba75ae13db365f026d3262f6f607e1677
vidal1101 <rodrigovidal278@gmail.com>
2022-09-17 17:37:56 -0600

    inicio del proyecto

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:app_notifications/main.dart';
 import 'package:app_notifications/pages/home_page.dart';
+import 'package:app_notifications/pages/message_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,12 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      initialRoute: 'home',
+      routes: {
+        'home'      : (context) => HomePage(),
+        'mensaje'   :(context) => Messagescreen(), 
+
+      },
     );
   }
 }
--- lib/pages/message_screen.dart
+++ lib/pages/message_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+
+class Messagescreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('test notifications'),
+        title: Text('mensaje'),
       ),
-      body: Center(child: Text('home screen')),
+      body:  Center(child: Text('hola mensaje')),
     );
   }
 }
