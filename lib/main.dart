35efcc5a31332ecc662142489267a75fd03fed20
vidal1101 <rodrigovidal278@gmail.com>
2022-09-18 17:09:20 -0600

    obteniendo el token del dispositivo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:app_notifications/services/custom_notificacion.dart';
 import 'package:flutter/material.dart';
 import 'package:app_notifications/app.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initializedApp();
+
   runApp(const MyApp());
 }
 
--- lib/services/custom_notificacion.dart
+++ lib/services/custom_notificacion.dart
@@ -1,2 +1,27 @@
 
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
 //sha1: 32:88:79:71:6E:D6:51:C4:A7:14:37:EE:39:9F:01:E5:23:BE:AF:C8
+
+
+class PushNotificationService {
+
+  static FirebaseMessaging messaging= FirebaseMessaging.instance;
+
+  static String? token ;
+
+
+
+  static Future initializedApp()async{
+    //push notifications 
+    await Firebase.initializeApp();
+
+    token = await FirebaseMessaging.instance.getToken();
+
+    print('token del dispositivo:  ${token}');
+
+    //local notifications
+  }
+
+
+}
